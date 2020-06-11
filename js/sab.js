3a17c9b49adafceef626dfd83db7149c4f64072f
David Jerome <david.jerome@payscale.com>
2020-06-11 10:08:05 -0400

    cleanup sab code

--- js/sab.js
+++ js/sab.js
@@ -1,62 +1,98 @@
+const companyCount = 3;
+
+const companyMap = {
+  "Company A": 0,
+  "Company B": 1,
+  "Company C": 2
+}
+
+const changeMap = {
+  Bear: {
+    roll2:  [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10],
+    roll3:  [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10],
+    roll4:  [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10],
+    roll5:  [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10],
+    roll6:  [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10],
+    roll7:  [-15,-2,-3,-4,-5,-6,-7,-8,-9,-10],
+    roll8:  [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10],
+    roll9:  [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10],
+    roll10: [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10],
+    roll11: [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10],
+    roll12: [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10]
+  },
+  Bull: {
+    roll2:  [1,2,3,4,5,6,7,8,9,10],
+    roll3:  [1,2,3,4,5,6,7,8,9,10],
+    roll4:  [1,2,3,4,5,6,7,8,9,10],
+    roll5:  [1,2,3,4,5,6,7,8,9,10],
+    roll6:  [1,2,3,4,5,6,7,8,9,10],
+    roll7:  [10,2,3,4,5,6,7,8,9,10],
+    roll8:  [1,2,3,4,5,6,7,8,9,10],
+    roll9:  [1,2,3,4,5,6,7,8,9,10],
+    roll10: [1,2,3,4,5,6,7,8,9,10],
+    roll11: [1,2,3,4,5,6,7,8,9,10],
+    roll12: [1,2,3,4,5,6,7,8,9,10]
+  }
+};
+
 function PriceRow(props) {
-  let previousPrice = null;
-  let currentPrice = null;
-  let change = "-";
+  const weeklyPrice = props.weeklyPrices[props.week] ? props.weeklyPrices[props.week] : props.weeklyPrices[props.week - 1]
+  let changeStyle = {};
   let changeSymbol = "";
-  let style = {};
+  let priceStyle = {};
 
-  if (props.prices[props.week] == null) {
-    previousPrice = props.prices[props.week - 2];
-    currentPrice = props.prices[props.week - 1];
-    if (props.week > 1) {
-      change = props.prices[props.week - 1] - props.prices[props.week - 2];
+  if (weeklyPrice.change != "-") {
+    if (weeklyPrice.change < 0) {
+      changeStyle = {backgroundColor: "Red", color: "White"};
     }
-  }
-  else {
-    previousPrice = props.prices[props.week - 1];
-    currentPrice = props.prices[props.week];
-    if (props.week > 0) {
-      change = props.prices[props.week] - props.prices[props.week -1];
-    }
-  }
-
-  if (change != "-") {
-    if (change < 0) {
-      style = {backgroundColor: "Red", color: "White"};
-    }
-    else if (change > 0) {
-      style = {backgroundColor: "Green", color: "White"};
+    else if (weeklyPrice.change > 0) {
+      changeStyle = {backgroundColor: "Green", color: "White"};
       changeSymbol = "+";
     }
     else {
-      style = {};
+      changeStyle = {};
     }
+  }
+
+  if (weeklyPrice.split) {
+    priceStyle = {backgroundColor: "Orange", color: "White"};
   }
 
   return (
     <tr>
       <td className="boldElement">{props.companyName}</td>
-      <td className="boldElement">{currentPrice}</td>
-      <td className="boldElement" style={style}>{changeSymbol + change}</td>
-      <td>{previousPrice}</td>      
+      <td className="boldElement" style={priceStyle}>{weeklyPrice.price}</td>
+      <td className="boldElement" style={changeStyle}>{changeSymbol + weeklyPrice.change}</td>
+      <td>{weeklyPrice.previousPrice}</td>      
     </tr>
   );
 }
 
 function HistoricalPriceRow(props) {
+  let historicalPrices = [];
+  let noDividendsStyle = {};
+
+  if (props.weeklyPrices[props.week] && props.weeklyPrices[props.week].noDividends) {
+    noDividendsStyle = {backgroundColor: "Gray"};
+  }
+
+  for (let i = 1; i <= 10; i++){
+    let priceStyle = {};
+    if (props.weeklyPrices[i]) {
+      if (props.weeklyPrices[i].split) {
+        priceStyle = {backgroundColor: "Orange", color: "White"};
+      }
+      historicalPrices.push(<td key={"historicalPrice" + i} style={priceStyle}>{props.weeklyPrices[i].price}</td>);
+    }
+    else {
+      historicalPrices.push(<td key={"historicalPrice" + i}>{null}</td>);
+    }
+  }
+
   return (
-    <tr>
+    <tr style={noDividendsStyle}>
       <td className="boldElement">{props.companyName}</td>
-      <td>{props.prices[1]}</td>
-      <td>{props.prices[2]}</td>
-      <td>{props.prices[3]}</td>
-      <td>{props.prices[4]}</td>
-      <td>{props.prices[5]}</td>
-      <td>{props.prices[6]}</td>
-      <td>{props.prices[7]}</td>
-      <td>{props.prices[8]}</td>
-      <td>{props.prices[9]}</td>
-      <td>{props.prices[10]}</td>
+      {historicalPrices}
     </tr>
   );
 }
@@ -94,41 +130,20 @@ class StocksAndBondsGame extends React.Component {
     this.state = {
       week: 0,
       market: "Bull",
-      diceRoll1: 3,
-      diceRoll2: 4,
+      dieValue1: 3,
+      dieValue2: 4,
       currentCompany: 0,
-      priceLists: Array(10).fill([100].concat(Array(10).fill(null)))
+      weeklyPriceLists: Array(10).fill([
+        {
+          price: 100,
+          change: "-",
+          previousPrice: null,
+          split: false,
+          noDividends: false
+        }
+      ])
     };
   }
-
-  changeMap = {
-    Bear: {
-      roll2:  [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10],
-      roll3:  [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10],
-      roll4:  [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10],
-      roll5:  [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10],
-      roll6:  [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10],
-      roll7:  [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10],
-      roll8:  [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10],
-      roll9:  [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10],
-      roll10: [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10],
-      roll11: [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10],
-      roll12: [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10]
-    },
-    Bull: {
-      roll2:  [1,2,3,4,5,6,7,8,9,10],
-      roll3:  [1,2,3,4,5,6,7,8,9,10],
-      roll4:  [1,2,3,4,5,6,7,8,9,10],
-      roll5:  [1,2,3,4,5,6,7,8,9,10],
-      roll6:  [1,2,3,4,5,6,7,8,9,10],
-      roll7:  [1,2,3,4,5,6,7,8,9,10],
-      roll8:  [1,2,3,4,5,6,7,8,9,10],
-      roll9:  [1,2,3,4,5,6,7,8,9,10],
-      roll10: [1,2,3,4,5,6,7,8,9,10],
-      roll11: [1,2,3,4,5,6,7,8,9,10],
-      roll12: [1,2,3,4,5,6,7,8,9,10]
-    }
-  };
 
   updateMarket() {
     this.setState({market: (this.state.market == "Bull") ? "Bear" : "Bull"});
@@ -136,29 +151,46 @@ class StocksAndBondsGame extends React.Component {
 
   rollDice() {
     this.setState({
-      diceRoll1: Math.floor(Math.random() * 6) + 1,
-      diceRoll2: Math.floor(Math.random() * 6) + 1
+      dieValue1: Math.floor(Math.random() * 6) + 1,
+      dieValue2: Math.floor(Math.random() * 6) + 1
     });
   }
 
   calculatePrice() {
     const currentWeek = this.state.currentCompany == 0 ? this.state.week + 1 : this.state.week;
-    const totalRoll = this.state.diceRoll1 + this.state.diceRoll2;
-    const priceLists = this.state.priceLists.slice();
-    const companyPriceList = this.state.priceLists[this.state.currentCompany].slice();
+    const totalRoll = this.state.dieValue1 + this.state.dieValue2;
+    const weeklyPriceLists = this.state.weeklyPriceLists.slice();
+    const companyPriceList = this.state.weeklyPriceLists[this.state.currentCompany].slice();
 
-    companyPriceList[currentWeek] = companyPriceList[currentWeek-1] + this.changeMap[this.state.market]["roll" + totalRoll][this.state.currentCompany];
+    const price = companyPriceList[currentWeek-1].price + changeMap[this.state.market]["roll" + totalRoll][this.state.currentCompany];
 
-    priceLists[this.state.currentCompany] = companyPriceList;
+    const weeklyPrice = {
+      price: price <= 150 ? (price < 0 ? 0 : price) : Math.ceil(price/2.0),
+      change: changeMap[this.state.market]["roll" + totalRoll][this.state.currentCompany],
+      previousPrice: companyPriceList[currentWeek - 1].price,
+      split: price > 150,
+      noDividends: price <= 50
+    };
+
+    companyPriceList.push(weeklyPrice);
+    weeklyPriceLists[this.state.currentCompany] = companyPriceList;
 
     this.setState({
-      priceLists: priceLists,
-      currentCompany: (this.state.currentCompany == 2) ? 0 : this.state.currentCompany + 1,
+      weeklyPriceLists: weeklyPriceLists,
+      currentCompany: (this.state.currentCompany == (companyCount - 1)) ? 0 : this.state.currentCompany + 1,
       week: currentWeek
     });
   }
 
   render() {
+    let currentPrices = [];
+    let historicalPrices = [];
+    for (let company in companyMap) {
+      const weeklyPrices = this.state.weeklyPriceLists[companyMap[company]];
+      currentPrices.push(<PriceRow key={"currentPriceRow" + companyMap[company]} companyName={company} weeklyPrices={weeklyPrices} week={this.state.week}/>);
+      historicalPrices.push(<HistoricalPriceRow key={"historicalPriceRow" + companyMap[company]} companyName={company} weeklyPrices={weeklyPrices} week={this.state.week}/>);
+    }
+
     return (
       <div className="grid-container">
         <main className="main">
@@ -179,9 +211,7 @@ class StocksAndBondsGame extends React.Component {
                     <tr>
                       <th>Company</th><th>Current Price</th><th>Change</th><th>Previous Price</th>
                     </tr>
-                    <PriceRow companyName="Company A" prices={this.state.priceLists[0]} week={this.state.week}/>
-                    <PriceRow companyName="Company B" prices={this.state.priceLists[1]} week={this.state.week}/>
-                    <PriceRow companyName="Company C" prices={this.state.priceLists[2]} week={this.state.week}/>
+                    {currentPrices}
                   </tbody>
                 </table>
               </div>
@@ -190,8 +220,8 @@ class StocksAndBondsGame extends React.Component {
             </div>
 
             <div className="current">
-              <h1>{this.state.diceRoll1 + this.state.diceRoll2}</h1>
-              <div><img src={"img/dice/" + this.state.diceRoll1 + ".png"} alt=""/><img src={"img/dice/" + this.state.diceRoll2 + ".png"} alt=""/></div>
+              <h1>{this.state.dieValue1 + this.state.dieValue2}</h1>
+              <div><img src={"img/dice/" + this.state.dieValue1 + ".png"} alt=""/><img src={"img/dice/" + this.state.dieValue2 + ".png"} alt=""/></div>
               <div><DiceRoll rollDice={() => this.rollDice()}/></div>
             </div>
           </div>
@@ -204,9 +234,7 @@ class StocksAndBondsGame extends React.Component {
                     <tr>
                       <th>Company</th><th>Week 1</th><th>Week 2</th><th>Week 3</th><th>Week 4</th><th>Week 5</th><th>Week 6</th><th>Week 7</th><th>Week 8</th><th>Week 9</th><th>Week 10</th>
                     </tr>
-                    <HistoricalPriceRow companyName="Company A" prices={this.state.priceLists[0]}/>
-                    <HistoricalPriceRow companyName="Company B" prices={this.state.priceLists[1]}/>
-                    <HistoricalPriceRow companyName="Company C" prices={this.state.priceLists[2]}/>
+                    {historicalPrices}
                   </tbody>
                 </table>
             </div>
