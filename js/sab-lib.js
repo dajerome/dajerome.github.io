0c3317ca15d76832294537b048c33d1cb810e1bf
David Jerome <david.jerome@payscale.com>
2020-07-13 17:55:40 -0400

    latest sab code

---
+++ js/sab-lib.js
@@ -0,0 +1,280 @@
+const marketCards = [
+  {
+    market: "Bear",
+    message: "Large terminal destroyed by fire; insufficient insurance on building due to Company's delayed move to new location.",
+    changes: {"Tri City Tran": -25}
+  },
+  {
+    market: "Bull",
+    message: "War scare promotes mixed activity on Wall Street.",
+    changes: {"Pioneer Mult": -8, "Stryker Drlg": 8, "Uranium Ent": 5}
+  },
+  {
+    market: "Bear",
+    message: "Land rights litigation holds up progress.",
+    changes: {"Stryker Drlg": -10}
+  },
+  {
+    market: "Bull",
+    message: "National firm leases Company's largest office building.",
+    changes: {"Metro Prop": 5}
+  },
+  {
+    market: "Bull",
+    message: "Company lands ten-year contract with large industrial equipment corporation.",
+    changes: {"Tri City Tran": 15}
+  },
+  {
+    market: "Bull",
+    message: "Influx of personnel of new company in nearby town creates a severe housing shortage.",
+    changes: {"Shady Brooks": 5}
+  },
+  {
+    market: "Bull",
+    message: "Intensive advertising campaign gains Company three major, long-term contracts.",
+    changes: {"Tri City Tran": 10}
+  },
+  {
+    market: "Bear",
+    message: "Company moves to a new excellent location.",
+    changes: {"Tri City Tran": 5}
+  },
+  {
+    market: "Bear",
+    message: "Extra year-end dividend of $2 per share declared by the Board of Directors. (Each stockholder receives $2 per share.)",
+    changes: {"Growth Corp": 10}
+  },
+  {
+    market: "Bull",
+    message: "United Auto announces new advanced-design auto entry in the mini-car field.",
+    changes: {"United Auto": 10}
+  },
+  {
+    market: "Bear",
+    message: "President, Vice-President and Chief Counsel of Growth Corporation of America reach retirement age.",
+    changes: {"Growth Corp": -10}
+  },
+  {
+    market: "Bull",
+    message: "Corporation releases high profit and sales financial report and announces plans to invest an additional $2 million on special research projects next year.",
+    changes: {"Growth Corp": 8}
+  },
+  {
+    market: "Bull",
+    message: "Major coal company announces reduced coal prices to electric power utilities.",
+    changes: {"Valley Power": 5}
+  },
+  {
+    market: "Bear",
+    message: "Community steadily deteriorates. The management is forced to lower rental rates to attract tenants.",
+    changes: {"Shady Brooks": -5}
+  },
+  {
+    market: "Bear",
+    message: "Two founders and major stockholders of the Corporation disagree on policy. One sells out his entire stockholdings.",
+    changes: {"Growth Corp": -8}
+  },
+  {
+    market: "Bear",
+    message: "Urban Renewal Program delayed by indecision of City Planning Commission.",
+    changes: {"Metro Prop": -10}
+  },
+  {
+    market: "Bear",
+    message: "Foreign car rage hits the buying public. Big cars in slow demand.",
+    changes: {"United Auto": -15}
+  },
+  {
+    market: "Bear",
+    message: "Surge of profit-taking drops stock market.",
+    changes: {"Growth Corp": -8, "Metro Prop": -5, "United Auto": -7}
+  },
+  {
+    market: "Bull",
+    message: "Corporation announces new metal forming process which it claims will revolutionize all metal-working industries covered by U.S. and foreign patents.",
+    changes: {"Growth Corp": 10}
+  },
+  {
+    market: "Bull",
+    message: "Experimental nuclear power station proves more economical than anticipated. Three electrical power companies announce plans to build large-scale nuclear power plants.",
+    changes: {"Uranium Ent": 10}
+  },
+  {
+    market: "Bull",
+    message: "Large petroleum corporation offers to buy all assets for cash. Offer is well above book value. Directors approve and will submit recommendation to stockholders.",
+    changes: {"Stryker Drlg": 17}
+  },
+  {
+    market: "Bear",
+    message: "Company's Annual Report shows net earnings off during fourth quarter.",
+    changes: {"Metro Prop": -5}
+  },
+  {
+    market: "Bear",
+    message: "Government suudenly announces it will no longer support ore prices, since it has large stockpiles.",
+    changes: {"Uranium Ent": -25}
+  },
+  {
+    market: "Bear",
+    message: "Internal Revenue depletion allowance reduced 50%.",
+    changes: {"Stryker Drlg": -15}
+  },
+  {
+    market: "Bull",
+    message: "Company prospectors find huge, new high-grade ore deposits.",
+    changes: {"Uranium Ent": 10}
+  },
+  {
+    market: "Bear",
+    message: "President hospitalized in sanitorium for an indefinite period.",
+    changes: {"Tri City Tran": -5}
+  },
+  {
+    market: "Bull",
+    message: "Buying wave raises the market.",
+    changes: {"Pioneer Mult": 3, "Valley Power": 4}
+  },
+  {
+    market: "Bull",
+    message: "President announces expansion plans to increase productive capacity 30%.",
+    changes: {"United Auto": 15}
+  },
+  {
+    market: "Bull",
+    message: "Commission grants permission to construct a new nuclear generating plant of great capacity and efficiency.",
+    changes: {"Valley Power": 5}
+  },
+  {
+    market: "Bull",
+    message: "General market rise over the last two months.",
+    changes: {"Growth Corp": 8, "Metro Prop": 5, "Pioneer Mult": 5, "United Auto": 7}
+  },
+  {
+    market: "Bear",
+    message: "Strikes halt production in all eight United Auto plants as UAW and Company officially fail to reach agreement on labor contract.",
+    changes: {"United Auto": -15}
+  },
+  {
+    market: "Bull",
+    message: "Corporation unexpectedly relinquishes its monopoly on its major product after a lengthy anti-trust suit.",
+    changes: {"Growth Corp": -10}
+  },
+  {
+    market: "Bull",
+    message: "Three-for-one split rumored.",
+    changes: {"United Auto": 10}
+  },
+  {
+    market: "Bear",
+    message: "Public Utility Commission rejects Company's request for rate hike.",
+    changes: {"Valley Power": -14}
+  },
+  {
+    market: "Bear",
+    message: "Competitor invents a new economical automatic transmission.",
+    changes: {"United Auto": -5}
+  },
+  {
+    market: "Bear",
+    message: "City Council considers the Company's choicest property for large industrial fair.",
+    changes: {"Metro Prop": 10}
+  }
+];
+
+const marketCalculator = {
+  roll2: {
+    Bear: [12, 14, 13, 10, 10, 20, 21, 25, 8],
+    Bull: [-2, -10, -7, -9, -2, -9, -7, -16, -4]
+  },
+  roll3: {
+    Bear: [7, -6, 10, -10, 30, 6, -19, 22, -2],
+    Bull: [26, 16, 25, 8, -14, 21, 14, -4, 17]
+  },
+  roll4: {
+    Bear: [9, 10, 7, -5, -20, 12, 21, 18, 7],
+    Bull: [18, 23, 11, 12, 46, 18, -5, 34, 15]
+  },
+  roll5: {
+    Bear: [7, 8, 5, -6, -40, 3, 16, -14, 4],
+    Bull: [23, 28, -2, 11, 56, 19, 30, 29, 14]
+  },
+  roll6: {
+    Bear: [8, 6, 4, -4, 40, 8, 4, -12, 3],
+    Bull: [20, 15, 15, 7, -20, 15, 13, -10, 12]
+  },
+  roll7: {
+    Bear: [6, 4, 3, 3, -15, 5, 8, -8, 5],
+    Bull: [17, 21, 13, -2, 37, 23, 23, 19, 14],
+  },
+  roll8: {
+    Bear: [5, 7, -1, -3, 45, 6, -10, 10, 4],
+    Bull: [19, 24, 17, 9, -5, 26, 13, -7, 15]
+  },
+  roll9: {
+    Bear: [-2, 6, -3, -8, -20, 7, 10, 14, 6],
+    Bull: [11, 18, 14, 11, 67, 15, 22, 18, 13]
+  },
+  roll10: {
+    Bear: [11, 11, -5, -7, 30, 10, -11, -18, -4],
+    Bull: [13, 31, 1, 14, -11, 18, 18, -14, 10]
+  },
+  roll11: {
+    Bear: [-5, 13, -8, 6, 25, 4, 18, -22, -4],
+    Bull: [14, -8, 19, -1, -9, 25, -10, 13, 19]
+  },
+  roll12: {
+    Bear: [-8, -10, -10, -15, -20, -20, -23, -25, -7],
+    Bull: [24, 24, 23, 20, 51, 27, 38, 33, 18]
+  }
+};
+
+const companies = [
+  {index: 0, shortName: "Growth Corp", fullName: "Growth Corporation of America", companyYield: "Yield 1%"},
+  {index: 1, shortName: "Metro Prop", fullName: "Metro Properties, Inc.", companyYield: "No Yield"},
+  {index: 2, shortName: "Pioneer Mult", fullName: "Pioneer Mutual Fund", companyYield: "Yield 4%"},
+  {index: 3, shortName: "Shady Brooks", fullName: "Shady Brooks Development", companyYield: "Yield 7%"},
+  {index: 4, shortName: "Stryker Drlg", fullName: "Stryker Drilling Company", companyYield: "No Yield"},
+  {index: 5, shortName: "Tri City Tran", fullName: "Tri-City Transport Company", companyYield: "No Yield"},
+  {index: 6, shortName: "United Auto", fullName: "United Auto Company", companyYield: "Yield 2%"},
+  {index: 7, shortName: "Uranium Ent", fullName: "Uranium Enterprises, Inc.", companyYield: "Yield 6%"},
+  {index: 8, shortName: "Valley Power", fullName: "Valley Power & Light Company", companyYield: "Yield 3%"}
+];
+
+class StocksAndBondsLib {
+  constructor () { }
+
+  // ========================================
+  getMarketCard = (index) => {
+    return marketCards[index];
+  }
+
+  // ========================================
+  getMarketChange = (roll, market, companyIndex) => {
+    return marketCalculator["roll" + roll][market][companyIndex];
+  }
+
+  // ========================================
+  getCompany = (index) => {
+    return companies[index];
+  }
+
+  // ========================================
+  getChangeStyle = (change) => {
+    let changeStyle = {};
+    let changeSymbol = "";
+
+    if (change != "-") {
+      if (change < 0) {
+        changeStyle = {backgroundColor: "Red", color: "White"};
+      }
+      else if (change > 0) {
+        changeStyle = {backgroundColor: "Green", color: "White"};
+        changeSymbol = "+";
+      }
+      else {
+        changeStyle = {};
+      }
+    }
+    return {changeStyle: changeStyle, changeSymbol: changeSymbol};
+  }
+}   
--- js/sab.js
+++ js/sab.js
@@ -1,86 +1,44 @@
+const lib = new StocksAndBondsLib();
+
+// ========================================
+
 const companyCount = 9;
+const marketCardsCount = 36;
+let dealtMarketCards = [];
 
-const companyMap = {
-  "Growth Corp": 0,
-  "Metro Prop": 1,
-  "Pioneer Mult": 2,
-  "Shady Brooks": 3,
-  "Stryker Drlg": 4,
-  "Tri City Tran": 5,
-  "United Auto": 6,
-  "Uranium Ent": 7,
-  "Valley Power": 8
-}
-
-const changeMap = {
-  Bear: {
-    roll2:  [12, 14, 13, 10, 10, 20, 21, 25, 8],
-    roll3:  [7, -6, 10, -10, 30, 6, -19, 22, -2],
-    roll4:  [9, 10, 7, -5, -20, 12, 21, 18, 7],
-    roll5:  [7, 8, 5, -6, -40, 3, 16, -14, 4],
-    roll6:  [8, 6, 4, -4, 40, 8, 4, -12, 3],
-    roll7:  [6, 4, 3, 3, -15, 5, 8, -8, 5],
-    roll8:  [5, 7, -1, -3, 45, 6, -10, 10, 4],
-    roll9:  [-2, 6, -3, -8, -20, 7, 10, 14, 6],
-    roll10: [11, 11, -5, -7, 30, 10, -11, -18, -4],
-    roll11: [-5, 13, -8, 6, 25, 4, 18, -22, -4],
-    roll12: [-8, -10, -10, -15, -20, -20, -23, -25, -7]
-  },
-  Bull: {
-    roll2:  [-2, -10, -7, -9, -2, -9, -7, -16, -4],
-    roll3:  [26, 16, 25, 8, -14, 21, 14, -4, 17],
-    roll4:  [18, 23, 11, 12, 46, 18, -5, 34, 15],
-    roll5:  [23, 28, -2, 11, 56, 19, 30, 29, 14],
-    roll6:  [20, 15, 15, 7, -20, 15, 13, -10, 12],
-    roll7:  [17, 21, 13, -2, 37, 23, 23, 19, 14],
-    roll8:  [19, 24, 17, 9, -5, 26, 13, -7, 15],
-    roll9:  [11, 18, 14, 11, 67, 15, 22, 18, 13],
-    roll10: [13, 31, 1, 14, -11, 18, 18, -14, 10],
-    roll11: [14, -8, 19, -1, -9, 25, -10, 13, 19],
-    roll12: [24, 24, 23, 20, 51, 27, 38, 33, 18]
-  }
-};
+// ========================================
 
 function PriceRow(props) {
   const weeklyPrice = props.weeklyPrices[props.week] ? props.weeklyPrices[props.week] : props.weeklyPrices[props.week - 1]
-  let changeStyle = {};
-  let changeSymbol = "";
+
+  let changeStyle = lib.getChangeStyle(weeklyPrice.change);
+  let marketChangeStyle = lib.getChangeStyle(weeklyPrice.marketChange);
+
   let priceStyle = {};
-
-  if (weeklyPrice.change != "-") {
-    if (weeklyPrice.change < 0) {
-      changeStyle = {backgroundColor: "Red", color: "White"};
-    }
-    else if (weeklyPrice.change > 0) {
-      changeStyle = {backgroundColor: "Green", color: "White"};
-      changeSymbol = "+";
-    }
-    else {
-      changeStyle = {};
-    }
-  }
-
   if (weeklyPrice.split) {
     priceStyle = {backgroundColor: "Orange", color: "White"};
   }
 
+  let rowStyle = {};
+  if (props.isCurrentCompany) {
+    rowStyle = {border: "1px solid black"};
+  }
+
   return (
-    <tr>
-      <td className="boldElement">{props.companyName}</td>
+    <tr style={rowStyle}>
+      <td className="boldElement">{props.company.shortName}</td>
       <td className="boldElement" style={priceStyle}>{weeklyPrice.price}</td>
-      <td className="boldElement" style={changeStyle}>{changeSymbol + weeklyPrice.change}</td>
+      <td className="boldElement" style={changeStyle.changeStyle}>{changeStyle.changeSymbol + weeklyPrice.change}</td>
+      <td className="boldElement" style={marketChangeStyle.changeStyle}>{marketChangeStyle.changeSymbol + weeklyPrice.marketChange}</td>
       <td>{weeklyPrice.previousPrice}</td>      
     </tr>
   );
 }
 
+// ========================================
+
 function HistoricalPriceRow(props) {
   let historicalPrices = [];
-  let noDividendsStyle = {};
-
-  if (props.weeklyPrices[props.week] && props.weeklyPrices[props.week].noDividends) {
-    noDividendsStyle = {backgroundColor: "Gray"};
-  }
 
   for (let i = 1; i <= 10; i++){
     let priceStyle = {};
@@ -95,54 +53,44 @@ function HistoricalPriceRow(props) {
     }
   }
 
+  let rowStyle = {};
+  if (props.weeklyPrices[props.week] && props.weeklyPrices[props.week].noDividends) {
+    rowStyle = {backgroundColor: "Gray"};
+  }
+
   return (
-    <tr style={noDividendsStyle}>
-      <td className="boldElement">{props.companyName}</td>
+    <tr style={rowStyle}>
+      <td className="boldElement">{props.company.shortName}</td>
       {historicalPrices}
     </tr>
   );
 }
 
-function MarketToggle(props) {
+// ========================================
+
+function StocksAndBondsButton(props) {
   return (
     <div>
-      <label className="switch">
-        <input type="checkbox" onChange={() => props.updateMarket()}/>
-        <span className="slider"></span>
-      </label>
+      <button className="button" onClick={() => props.f()}>{props.txt}</button>
     </div>
   );
 }
 
-function DiceRoll(props) {
-  return (
-    <div>
-      <button className="button" onClick={() => props.rollDice()}>Roll Dice</button>
-    </div>
-  );
-}
-
-function PriceReveal(props) {
-  return (
-    <div>
-      <button className="button" onClick={() => props.calculatePrice()}>Reveal Price</button>
-    </div>
-  );
-}
+// ========================================
 
 class StocksAndBondsGame extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       week: 0,
-      market: "Bull",
-      dieValue1: 3,
-      dieValue2: 4,
+      marketCard: {market: "Bull", message: null, changes: []},
+      dice: {dieValue1: 3, dieValue2: 4},
       currentCompany: 0,
       weeklyPriceLists: Array(10).fill([
         {
           price: 100,
           change: "-",
+          marketChange: "-",
           previousPrice: null,
           split: false,
           noDividends: false
@@ -151,28 +99,55 @@ class StocksAndBondsGame extends React.Component {
     };
   }
 
-  updateMarket() {
-    this.setState({market: (this.state.market == "Bull") ? "Bear" : "Bull"});
+// ========================================
+
+  drawMarketCard() {
+    if (dealtMarketCards.length == marketCardsCount) {
+      this.setState({
+        marketCard: {market: null, message: "out of market cards", changes: []}
+      });
+    }
+    else{
+      let r = Math.floor(Math.random() * marketCardsCount);
+      while (dealtMarketCards.includes(r)) {
+        r = Math.floor(Math.random() * marketCardsCount);
+      }
+      dealtMarketCards.push(r);
+
+      let marketCard = lib.getMarketCard(r);
+      this.setState({
+        marketCard: marketCard
+      });
+    }
   }
+
+// ========================================
 
   rollDice() {
     this.setState({
-      dieValue1: Math.floor(Math.random() * 6) + 1,
-      dieValue2: Math.floor(Math.random() * 6) + 1
+      dice: {dieValue1: Math.floor(Math.random() * 6) + 1, dieValue2: Math.floor(Math.random() * 6) + 1}
     });
   }
 
+// ========================================
+
   calculatePrice() {
     const currentWeek = this.state.currentCompany == 0 ? this.state.week + 1 : this.state.week;
-    const totalRoll = this.state.dieValue1 + this.state.dieValue2;
+    const totalRoll = this.state.dice.dieValue1 + this.state.dice.dieValue2;
     const weeklyPriceLists = this.state.weeklyPriceLists.slice();
     const companyPriceList = this.state.weeklyPriceLists[this.state.currentCompany].slice();
 
-    const price = companyPriceList[currentWeek-1].price + changeMap[this.state.market]["roll" + totalRoll][this.state.currentCompany];
+    const company = lib.getCompany(this.state.currentCompany);
+    const companyName = company.shortName;
+
+    const marketChange = this.state.marketCard.changes.hasOwnProperty(companyName) ? this.state.marketCard.changes[companyName] : 0;
+
+    const price = companyPriceList[currentWeek-1].price + lib.getMarketChange(totalRoll, this.state.marketCard.market, this.state.currentCompany) + marketChange;
 
     const weeklyPrice = {
       price: price <= 150 ? (price < 0 ? 0 : price) : Math.ceil(price/2.0),
-      change: changeMap[this.state.market]["roll" + totalRoll][this.state.currentCompany],
+      change: lib.getMarketChange(totalRoll, this.state.marketCard.market, this.state.currentCompany),
+      marketChange: marketChange != 0 ? marketChange : "-",
       previousPrice: companyPriceList[currentWeek - 1].price,
       split: price > 150,
       noDividends: price <= 50
@@ -188,13 +163,38 @@ class StocksAndBondsGame extends React.Component {
     });
   }
 
+// ========================================
+
   render() {
     let currentPrices = [];
     let historicalPrices = [];
-    for (let company in companyMap) {
-      const weeklyPrices = this.state.weeklyPriceLists[companyMap[company]];
-      currentPrices.push(<PriceRow key={"currentPriceRow" + companyMap[company]} companyName={company} weeklyPrices={weeklyPrices} week={this.state.week}/>);
-      historicalPrices.push(<HistoricalPriceRow key={"historicalPriceRow" + companyMap[company]} companyName={company} weeklyPrices={weeklyPrices} week={this.state.week}/>);
+    let marketChanges = [];
+    for (let i = 0; i < companyCount; i++) {
+      const company = lib.getCompany(i);
+      const weeklyPrices = this.state.weeklyPriceLists[i];
+
+      currentPrices.push(
+        <PriceRow
+          key={"currentPriceRow" + i}
+          company={company}
+          weeklyPrices={weeklyPrices}
+          week={this.state.week}
+          isCurrentCompany={i==this.state.currentCompany}
+        />
+      );
+
+      historicalPrices.push(
+        <HistoricalPriceRow
+          key={"historicalPriceRow" + i}
+          company={company}
+          weeklyPrices={weeklyPrices}
+          week={this.state.week}
+        />
+      );
+    }
+
+    for (let companyName in this.state.marketCard.changes) {
+      marketChanges.push(<li key={companyName}>{companyName} : {this.state.marketCard.changes[companyName]}</li>)
     }
 
     return (
@@ -203,9 +203,11 @@ class StocksAndBondsGame extends React.Component {
 
           <div className="main-current">
             <div className="current">
-              <h1>{this.state.market} Market</h1>
-              <div><img width="150px" height="150px" src={"img/market/" + this.state.market + ".png"} alt=""/></div>
-              <div><MarketToggle updateMarket={() => this.updateMarket()}/></div>
+              <h1>{this.state.marketCard.market} Market</h1>
+              <div><img width="150px" height="150px" src={this.state.marketCard.market ? "img/market/" + this.state.marketCard.market + ".png" : null} alt=""/></div>
+              <div style={{textAlign: "center", fontSize: "18px"}}>{this.state.marketCard.message}</div>
+              <div style={{textAlign: "center", fontSize: "18px"}}><ul>{marketChanges}</ul></div>
+              <div><StocksAndBondsButton f={() => this.drawMarketCard()} txt={"Draw Market Card"}/></div>
             </div>
 
             <div className="current">
@@ -215,20 +217,20 @@ class StocksAndBondsGame extends React.Component {
                 <table>
                   <tbody>
                     <tr>
-                      <th>Company</th><th>Current Price</th><th>Change</th><th>Previous Price</th>
+                      <th>Company</th><th>Current Price</th><th>Change</th><th>Situation Change</th><th>Previous Price</th>
                     </tr>
                     {currentPrices}
                   </tbody>
                 </table>
               </div>
               <br/>
-              <div><PriceReveal calculatePrice={() => this.calculatePrice()}/></div>
+              <div><StocksAndBondsButton f={() => this.calculatePrice()} txt={"Reveal Price"}/></div>
             </div>
 
             <div className="current">
-              <h1>{this.state.dieValue1 + this.state.dieValue2}</h1>
-              <div><img src={"img/dice/" + this.state.dieValue1 + ".png"} alt=""/><img src={"img/dice/" + this.state.dieValue2 + ".png"} alt=""/></div>
-              <div><DiceRoll rollDice={() => this.rollDice()}/></div>
+              <h1>{this.state.dice.dieValue1 + this.state.dice.dieValue2}</h1>
+              <div><img src={"img/dice/" + this.state.dice.dieValue1 + ".png"} alt=""/><img src={"img/dice/" + this.state.dice.dieValue2 + ".png"} alt=""/></div>
+              <div><StocksAndBondsButton f={() => this.rollDice()} txt={"Roll Dice"}/></div>
             </div>
           </div>
 
@@ -241,17 +243,8 @@ class StocksAndBondsGame extends React.Component {
                       <th>Company</th><th>Week 1</th><th>Week 2</th><th>Week 3</th><th>Week 4</th><th>Week 5</th><th>Week 6</th><th>Week 7</th><th>Week 8</th><th>Week 9</th><th>Week 10</th>
                     </tr>
                     <tr>
-                      <td className="boldElement">C City Bonds<br/>Yield 5%</td>
-                      <td>PAR</td>
-                      <td>PAR</td>
-                      <td>PAR</td>
-                      <td>PAR</td>
-                      <td>PAR</td>
-                      <td>PAR</td>
-                      <td>PAR</td>
-                      <td>PAR</td>
-                      <td>PAR</td>
-                      <td>PAR</td>
+                      <td className="boldElement">C City Bonds</td>
+                      <td>PAR</td><td>PAR</td><td>PAR</td><td>PAR</td><td>PAR</td><td>PAR</td><td>PAR</td><td>PAR</td><td>PAR</td><td>PAR</td>
                     </tr>
                     {historicalPrices}
                   </tbody>
